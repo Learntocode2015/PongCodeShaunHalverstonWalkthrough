5fad563883718e1ba749fbedda2c2a011e352b2f
user <user@USERAB96>
2022-02-13 17:03:39 +0000

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace PongCodeShaunHalverston
+{
+    partial class Pong
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.player1 = new System.Windows.Forms.PictureBox();
+            this.pongBall = new System.Windows.Forms.PictureBox();
+            this.cpuScoreLabel = new System.Windows.Forms.Label();
+            this.playerScoreLabel = new System.Windows.Forms.Label();
+            this.cpuPlayer = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.player1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pongBall)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cpuPlayer)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // player1
+            // 
+            this.player1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.player1.Location = new System.Drawing.Point(92, 162);
+            this.player1.Name = "player1";
+            this.player1.Size = new System.Drawing.Size(32, 291);
+            this.player1.TabIndex = 0;
+            this.player1.TabStop = false;
+            // 
+            // pongBall
+            // 
+            this.pongBall.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.pongBall.Location = new System.Drawing.Point(877, 280);
+            this.pongBall.Name = "pongBall";
+            this.pongBall.Size = new System.Drawing.Size(28, 28);
+            this.pongBall.TabIndex = 2;
+            this.pongBall.TabStop = false;
+            // 
+            // cpuScoreLabel
+            // 
+            this.cpuScoreLabel.Location = new System.Drawing.Point(1509, 48);
+            this.cpuScoreLabel.Name = "cpuScoreLabel";
+            this.cpuScoreLabel.Size = new System.Drawing.Size(35, 35);
+            this.cpuScoreLabel.TabIndex = 4;
+            this.cpuScoreLabel.Text = "0";
+            // 
+            // playerScoreLabel
+            // 
+            this.playerScoreLabel.Location = new System.Drawing.Point(135, 27);
+            this.playerScoreLabel.Name = "playerScoreLabel";
+            this.playerScoreLabel.Size = new System.Drawing.Size(35, 35);
+            this.playerScoreLabel.TabIndex = 5;
+            this.playerScoreLabel.Text = "0";
+            // 
+            // cpuPlayer
+            // 
+            this.cpuPlayer.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.cpuPlayer.Cursor = System.Windows.Forms.Cursors.AppStarting;
+            this.cpuPlayer.Location = new System.Drawing.Point(1674, 162);
+            this.cpuPlayer.Name = "cpuPlayer";
+            this.cpuPlayer.Size = new System.Drawing.Size(32, 291);
+            this.cpuPlayer.TabIndex = 6;
+            this.cpuPlayer.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 20;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Pong
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 32F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(2000, 750);
+            this.Controls.Add(this.cpuPlayer);
+            this.Controls.Add(this.playerScoreLabel);
+            this.Controls.Add(this.cpuScoreLabel);
+            this.Controls.Add(this.pongBall);
+            this.Controls.Add(this.player1);
+            this.Name = "Pong";
+            this.Text = "pong";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Pong_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Pong_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.player1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pongBall)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cpuPlayer)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private PictureBox player1;
+        private PictureBox pongBall;
+        private Label cpuScoreLabel;
+        private Label playerScoreLabel;
+        private PictureBox cpuPlayer;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,124 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PongCodeShaunHalverston
+{
+    public partial class Pong : Form
+    {
+        int cpuDirection = 5;
+        int ballXCoordinate = 5;
+        int ballYCoordinate = 5;
+        int playerScore2 =0;
+        int cpuScore2 = 0;
+        int bottomBoundary;
+        int centerPoint;
+        int xMidpoint;
+        int yMidpoint;
+        bool playerDetectedUp;
+        bool playerDetectedDown;
+        int spaceBarClicked = 0;
+        public Pong()
+        {
+            InitializeComponent();
+            bottomBoundary = ClientSize.Height - player1.Height;
+            
+            xMidpoint = ClientSize.Width / 2;
+            yMidpoint = ClientSize.Height / 2;   
+        }
+
+        private void pong_Load(object sender, EventArgs e)
+        { 
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Random newBallSpot = new Random();
+            int newSpot = newBallSpot.Next(100, ClientSize.Height - 100);
+            //Adjust where the ball is every tick
+            pongBall.Top -= ballYCoordinate;
+            pongBall.Left -= ballXCoordinate;
+            //make the CPU move
+            cpuPlayer.Top += cpuDirection;
+            //check if CPU has reached the top or the bottom
+            if (cpuPlayer.Top < 0 || cpuPlayer.Top > bottomBoundary)
+                {
+                cpuDirection = -cpuDirection;
+                }
+            // Check if the ball has exited the left side of the screen
+            if (pongBall.Left < 0)
+            {
+                pongBall.Left = xMidpoint;
+                pongBall.Top = newSpot;
+                ballXCoordinate = -ballXCoordinate;
+                cpuScore2++;
+                cpuScoreLabel.Text = cpuScore2.ToString();
+            }
+
+            // Check if the ball has exited the right side of the screen
+            if (pongBall.Left + pongBall.Width > ClientSize.Width)
+            {
+                pongBall.Left = xMidpoint;
+                pongBall.Top = newSpot;
+                ballXCoordinate = -ballXCoordinate;
+                playerScore2++;
+                playerScoreLabel.Text = playerScore2.ToString();
+            }
+            // Ensure the ball is within the boundaries of the screen
+            if (pongBall.Top < 0 || pongBall.Top + pongBall.Height > ClientSize.Height) { ballYCoordinate = -ballYCoordinate; }
+
+            // Check if the ball hits the player or CPU paddle
+            if (pongBall.Bounds.IntersectsWith(player1.Bounds) || pongBall.Bounds.IntersectsWith(cpuPlayer.Bounds))
+            {
+                // Send ball opposite direction
+                ballXCoordinate = -ballXCoordinate;
+            }
+
+            // Move player up
+
+            if (playerDetectedUp == true && player1.Top > 0) { player1.Top -= 10; }
+            // Move player down
+            if (playerDetectedDown == true && player1.Top < bottomBoundary) { player1.Top += 10; }
+            // Check for winner
+            if (playerScore2 >= 10) { timer1.Stop(); }
+        }
+
+
+        private void Pong_KeyDown(object sender, KeyEventArgs e)
+        {
+            // If player presses the up arrow, move paddle upwards
+            if (e.KeyCode == Keys.Up) { playerDetectedUp = true; }
+            // If player presses the down arrow, move paddle downwards
+            if (e.KeyCode == Keys.Down) { playerDetectedDown = true; }
+            // If player presses the C key, open the character selection screen
+
+            // If player presses space bar, pause the game
+            if (e.KeyCode == Keys.Space)
+            {
+                if (spaceBarClicked % 2 == 0)
+                {
+                    timer1.Stop();
+                }
+                else
+                {
+                    timer1.Start();
+                }
+            }
+            spaceBarClicked++;
+        }
+
+        private void Pong_KeyUp(object sender, KeyEventArgs e)
+        {
+            
+            if (e.KeyCode == Keys.Up) { playerDetectedUp = false; }
+            if (e.KeyCode == Keys.Down) { playerDetectedDown = false; }
+            
+        }
+    }
+}
